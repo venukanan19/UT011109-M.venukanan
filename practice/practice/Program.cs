8b8be1ef4e788cc33a50b0e106616c75fda474c5
Manokaralingam Venukanan <UT011109@unicomtic.com>
2026-05-18 14:29:38 +0530

    first commit

---
+++ practice/practice/Program.cs
@@ -0,0 +1,83 @@
+﻿
+namespace LearnLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            BankAccount adam = new BankAccount();
+
+            adam.AccountNumber = "1000A";
+            adam.Balance = 1000.00m;
+
+
+
+            adam.Deposit(2000.00m);
+            
+
+            adam.Deposit(1000.00m);
+           
+
+            adam.Deposit(2000.00m);
+            adam.PrintStatement();
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class BankAccount
+    {
+        public string AccountNumber;
+        public decimal Balance;
+
+
+        List<Transaction> Transactions = new List<Transaction>();
+
+
+        public void Deposit(decimal amount)
+        {
+            Balance = Balance + amount;
+
+            Transaction t = new Transaction
+            {
+                TransactionType = "Deposit",
+                Date = DateTime.Now,
+                Balance = Balance
+            };
+
+            Transactions.Add(t);
+
+
+        }
+
+        public void PrintStatement()
+        {
+            foreach (var t in Transactions)
+            {
+                Console.WriteLine($"{t.Balance}  {t.TransactionType}  {t.Date}");
+            }
+
+        }
+
+
+    }
+
+    public class Transaction
+    {
+        public int TransactionID;
+
+        public string TransactionType;
+
+        public DateTime Date;
+
+        public decimal Balance;
+    }
+
+}
